1df26ab1d1d9b2b0865b5bf5475f5d2733163bed
Mike Amundsen <mamund@yahoo.com>
2018-12-26 17:18:17 -0500

    dynaic port support

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+/*****************************************
+ * sample Express app
+ * 2019-01 mamund
+ *****************************************/
+
 var express = require('express');
 var app = express();
 var birds = require('./birds');
-const port = 8181;
+var port = process.env.PORT || 8181;
 
 app.use('/birds',birds);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
